429715fea26c0ac643d682c70907b03eccfddbe0
Yevhen Malyshko <memx86@gmail.com>
2022-01-17 15:28:08 +0200

    practice1

---
+++ src/js/practice1.js
@@ -0,0 +1,14 @@
+// Check number: 200
+// Tip: 15%
+// Tip amount: 30
+// Total sum to pay:  230
+
+const check = Number.parseFloat(prompt('Please enter check number'));
+const tip = Number.parseFloat(prompt('Please enter tip amount in %'));
+const tipAmount = (check * tip) / 100;
+const totalPrice = check + tipAmount;
+alert(`Check number: ${check.toFixed(2)}
+Tip: ${tip.toFixed(2)}%
+Tip amount: ${tipAmount.toFixed(2)}
+Total sum to pay:  ${totalPrice.toFixed(2)}
+`);
