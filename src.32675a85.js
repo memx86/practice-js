71f3439b909c1ee048a568ca3157a1f557964360
memx86 <93256591+memx86@users.noreply.github.com>
2022-01-17 13:29:35 +0000

    Deploying to gh-pages from @ memx86/practice-js@429715fea26c0ac643d682c70907b03eccfddbe0 🚀

--- src.32675a85.js
+++ src.32675a85.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-00/src.a3c36ac9.js.map
+},{}],"yE6p":[function(require,module,exports) {
+const e=Number.parseFloat(prompt("Please enter check number")),t=Number.parseFloat(prompt("Please enter tip amount in %")),o=e*t/100,n=e+o;alert(`Check number: ${e.toFixed(2)}\nTip: ${t.toFixed(2)}%\nTip amount: ${o.toFixed(2)}\nTotal sum to pay:  ${n.toFixed(2)}\n`);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/practice1");
+},{"./sass/main.scss":"clu1","./js/practice1":"yE6p"}]},{},["Focm"], null)
+//# sourceMappingURL=/practice-js/src.32675a85.js.map
