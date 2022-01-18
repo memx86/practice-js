ad1c1a1a8d63beeb3668972a6551c11c7211fc48
Yevhen Malyshko <memx86@gmail.com>
2022-01-18 21:08:59 +0200

    task 3 GUI

--- src/js/practice2.js
+++ src/js/practice2.js
@@ -125,38 +125,120 @@
 
 // ==============================================================Code:
 
-let doWant = confirm('Do you want to play a game ?');
-if (!doWant) {
-  alert('You did not become a billionaire, but can.');
-} else {
-  let gameNumber = 1;
-  let prize = 0;
-  do {
-    let range = 5 * gameNumber;
-    const number = Math.round(Math.random() * range);
-    let win = false;
-    // console.log(number);
-    for (let i = 0; i < 3; i += 1) {
-      const maxPrize = (100 * gameNumber) / Math.pow(2, i);
-      const choice = Number(
-        prompt(`
-Choose a roulette pocket number from 0 to ${range}
-Attempts left: ${3 - i}
-Total prize: ${prize}$
-Possible prize on current attempt: ${maxPrize}
-`),
-      );
-      if (choice === number) {
-        win = true;
-        prize = +maxPrize;
-        break;
-      }
-    }
-    if (!win) {
-      alert(`Thank you for your participation. Your prize is: ${prize}$`);
-    } else alert(`Congratulation, you won! Your prize is: ${prize}$`);
-    doWant = confirm('Do you want to play again ?');
-    gameNumber = +1;
-  } while (doWant);
-  alert(`Thank you for your participation. Your prize is: ${prize}$`);
+// let doWant = confirm('Do you want to play a game ?');
+// if (!doWant) {
+//   alert('You did not become a billionaire, but can.');
+// } else {
+//   let gameNumber = 1;
+//   let prize = 0;
+//   do {
+//     let range = 5 * gameNumber;
+//     const number = Math.round(Math.random() * range);
+//     let win = false;
+//     // console.log(number);
+//     for (let i = 0; i < 3; i += 1) {
+//       const maxPrize = (100 * gameNumber) / Math.pow(2, i);
+//       const choice = Number(
+//         prompt(`
+// Choose a roulette pocket number from 0 to ${range}
+// Attempts left: ${3 - i}
+// Total prize: ${prize}$
+// Possible prize on current attempt: ${maxPrize}
+// `),
+//       );
+//       if (choice === number) {
+//         win = true;
+//         prize = +maxPrize;
+//         break;
+//       }
+//     }
+//     if (!win) {
+//       alert(`Thank you for your participation. Your prize is: ${prize}$`);
+//     } else alert(`Congratulation, you won! Your prize is: ${prize}$`);
+//     doWant = confirm('Do you want to play again ?');
+//     gameNumber = +1;
+//   } while (doWant);
+//   alert(`Thank you for your participation. Your prize is: ${prize}$`);
+// }
+
+const refs = {
+  game: document.querySelector('.game'),
+  btnYes: document.querySelector('.yes'),
+  btnNo: document.querySelector('.no'),
+  form: document.querySelector('.choice'),
+  input: document.querySelector('.choice input'),
+  msg: document.querySelector('.message'),
+  range: document.querySelector('.range'),
+  attempts: document.querySelector('.attempts'),
+  prize: document.querySelector('.prize'),
+  maxPrize: document.querySelector('.maxPrize'),
+};
+let gameNumber = 1;
+let prize = 0;
+let roulette = -1;
+let tryNumber = 1;
+let range = 5;
+let attempts = 3;
+let maxPrize = (100 * gameNumber) / Math.pow(2, tryNumber - 1);
+
+refs.btnYes.addEventListener('click', onYes);
+refs.btnNo.addEventListener('click', onNo);
+function onYes() {
+  refs.msg.textContent = "Let's play!";
+  showGame();
+  if (gameNumber === 1) refs.form.addEventListener('submit', onTry);
+  updateGameUI({ range, attempts, prize, maxPrize });
+}
+function onNo() {
+  if (gameNumber === 1) {
+    refs.msg.textContent = 'You did not become a billionaire, but can.';
+  } else refs.msg.textContent = `Thank you for your participation. Your prize is: ${prize}$`;
+  hideGame();
+}
+function hideGame() {
+  refs.game.classList.add('is-hidden');
+}
+function showGame() {
+  refs.game.classList.remove('is-hidden');
+}
+function onTry(e) {
+  e.preventDefault();
+  if (tryNumber > 3) {
+    return;
+  }
+  const choice = Number(e.target.elements.num.value);
+  attempts = 3 - tryNumber;
+  if (roulette < 0) roulette = Math.round(Math.random() * range);
+  console.log(roulette);
+  maxPrize = (100 * gameNumber) / Math.pow(2, tryNumber - 1);
+
+  if (choice === roulette) {
+    prize += maxPrize;
+    endCurrentGame(`Congratulation, you won! Your prize is: ${prize}$`);
+    return;
+  }
+  if (tryNumber === 3) {
+    endCurrentGame(`Thank you for your participation. Your prize is: ${prize}$`);
+    return;
+  }
+  const nextMaxPrize = (100 * gameNumber) / Math.pow(2, tryNumber);
+  tryNumber += 1;
+  updateGameUI({ range, attempts, prize, maxPrize: nextMaxPrize });
+}
+function updateGameUI({ range, attempts, prize, maxPrize }) {
+  refs.range.textContent = range;
+  refs.attempts.textContent = attempts;
+  refs.prize.textContent = prize;
+  refs.maxPrize.textContent = maxPrize;
+}
+function endCurrentGame(message) {
+  hideGame();
+  gameNumber += 1;
+  tryNumber = 1;
+  refs.msg.textContent = message;
+  refs.input.max = 5 * gameNumber;
+  attempts = 3;
+  range = 5 * gameNumber;
+  maxPrize = 100 * gameNumber;
+  if (gameNumber > 1) roulette = Math.round(Math.random() * range);
 }
