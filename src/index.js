eb40a0a56acce300de66ef48bc9b5e05b248f097
Yevhen Malyshko <memx86@gmail.com>
2022-01-21 16:00:28 +0200

    practice 5

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './sass/main.scss';
 // import './js/practice1';
 import './js/practice2';
 // import './js/practice3';
-import './js/practice4';
+// import './js/practice4';
+import './js/practice5';
